b887fd23d6b03e7a0ede41579d4a43ab34e907ff
Zixuan Wang <wzxllx@gmail.com>
2016-11-07 19:13:25 +0800

    Init lab3.

---
+++ HBaseAndHive/src/main/java/nju/HBaseAndHive.java
@@ -0,0 +1,13 @@
+package nju;
+
+/**
+ * Hello world!
+ *
+ */
+public class HBaseAndHive
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
